da72ea34010d5c447ceda32842f8cea19ceae239
Tanya Bihag <tbihag@gmail.com>
2019-09-08 03:15:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
-# Add your code here
+class Dog 
+  @@all = [ ]
+  @@names = [ ]
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@names << self.name
+    self.save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    names_list = [ ]
+    @@names.each do |thing|
+      if names_list.include?(thing) == false
+        names_list << "#{thing}"
+      end
+    end
+  puts names_list
+end
+
+  def self.clear_all
+    @@all.clear
+    @@names.clear
+  end
+end 
